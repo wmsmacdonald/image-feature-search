fbd42564d521b4f25f02a70697443ae37c69870a
William MacDonald <wmsmacdonald@gmail.com>
2017-05-18 02:48:25 -0500

    store as bytes

--- indexer.py
+++ indexer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
-import cv2
 import sys
 from functools import partial as p
 import os
 import pickle
 from get_descriptors import get_descriptors
+import itertools
+import numpy as np
 
 
 def serialize_keypoints(keypoints, descriptors):
@@ -13,12 +14,35 @@ def serialize_keypoints(keypoints, descriptors):
             for kp, desc in zip(keypoints, descriptors)]
 
 
+def map_3d(func, iterable):
+    return list(map(lambda row: list(map(func, row)), iterable))
+
+
+def compose(*functions):
+    def inner(arg):
+        for f in reversed(functions):
+            arg = f(arg)
+        return arg
+    return inner
+
+
+bytes_descriptor_functions = [
+    bytes,
+    itertools.chain.from_iterable,
+    p(map, np.matrix.tolist),
+    p(map, np.matrix.flatten)
+]
+
+bytes_descriptor = compose(*bytes_descriptor_functions)
+
 files = sorted(os.listdir(sys.argv[1]))
 file_paths = map(p(os.path.join, sys.argv[1]), files)
 
 descriptors_by_file = list(map(get_descriptors, file_paths))
 
-database = [(file, kp) for file, kp in zip(files, descriptors_by_file)]
+bytes_descriptors_by_file = list(map(bytes_descriptor, descriptors_by_file))
+
+database = [(file, kp) for file, kp in zip(files, bytes_descriptors_by_file)]
 
 pickle.dump(database, open(sys.argv[2], 'wb'))
 
--- search.py
+++ search.py
@@ -6,10 +6,15 @@ from functools import partial as p
 import sys
 import operator as op
 from get_descriptors import get_descriptors
+import numpy as np
 
 # FLANN parameters
 FLANN_INDEX_KDTREE = 0
-index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+FLANN_INDEX_LSH = 6
+index_params= dict(algorithm = FLANN_INDEX_LSH,
+                   table_number = 6, # 12
+                   key_size = 12,     # 20
+                   multi_probe_level = 1) #2
 search_params = dict(checks=50)   # or pass empty dictionary
 
 
@@ -17,20 +22,27 @@ def compute_distance(des1, des2):
     flann = cv2.FlannBasedMatcher(index_params, search_params)
 
     matches = flann.knnMatch(des1, des2, k=1)
-    return sum(map(lambda m: m[0].distance, matches))
+    return sum(map(lambda m: m[0].distance, matches)) / len(matches)
 
 
 original_descriptors = get_descriptors(sys.argv[1])
 
+
+def descriptors(byte_descriptors):
+    a = np.frombuffer(byte_descriptors, dtype=np.uint8)
+    return a.reshape(original_descriptors.shape)
+
 database = pickle.load(open(sys.argv[2], 'rb'))
 
-files, descriptors_by_file = zip(*database)
+files, descriptor_bytes_by_file = zip(*database)
+
+descriptors_by_file = map(descriptors, descriptor_bytes_by_file)
 
 distances_by_file = map(p(compute_distance, original_descriptors), descriptors_by_file)
 
 results = sorted(zip(files, distances_by_file), key=op.itemgetter(1))
 
 for r in results:
-    print("File: %s, Distance: %d" % r)
+    print("File: %s, Distance: %f" % r)
 
 
