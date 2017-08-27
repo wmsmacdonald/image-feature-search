8a53b193d687a2194b0d147125070e3cd120b8eb
William MacDonald <wmsmacdonald@gmail.com>
2017-08-26 20:53:39 -0700

    keypoint sig

--- keypoint_signatures.py
+++ keypoint_signatures.py
@@ -1,5 +1,6 @@
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
+import math
 import sys
 import cv2
 from matplotlib import pyplot as plt
@@ -8,58 +9,37 @@ from functools import partial as p
 DESCRIPTOR_SIZE = 16
 
 
-def compute_neighbor_means(keypoint_coordinates):
-    if len(keypoint_coordinates) == 0:
-        return []
+def compute_grid_coordinates(center_x, center_y, grid_point_separation, square_offset=5):
+    distance_offset = grid_point_separation * square_offset
 
-    nearest_neighbors = NearestNeighbors(n_neighbors=min(len(keypoint_coordinates), 11))
-    nearest_neighbors.fit(keypoint_coordinates)
-    indexes_by_keypoints = nearest_neighbors.kneighbors(keypoint_coordinates, return_distance=False)
-    neighbors_by_keypoints = [[keypoint_coordinates[index] for index in indexes[1:]] for indexes in indexes_by_keypoints]
-    means_by_keypoint = [np.mean(neighbors, axis=0, dtype=int) for neighbors in neighbors_by_keypoints]
-    return means_by_keypoint
+    xs = range(center_x - distance_offset, center_x + distance_offset + 1, grid_point_separation)
+    ys = range(center_y - distance_offset, center_y + distance_offset + 1, grid_point_separation)
+
+    return [(x, y) for y in ys for x in xs]
 
 
-def compute_surrounding_coordinates(center, neighbors_mean):
-    difference_vector = neighbors_mean - center
+def compute_grid_averages(coordinates, image, p):
+    grid_square_offset_lower = (p - 1) // 2
+    grid_square_offset_upper = p // 2
 
-    rotations = np.array([[1, 1], [1, -1], [-1, 1], [-1, -1]])
-    scales = np.array([[1, 1], [2, 2], [3, 3]])
+    def threshold_y(value):
+        return max(0, min(value, image.shape[0] - 1))
 
-    rotated = np.multiply(rotations, difference_vector)
-    scaled = [vector for unit_vector in rotated for vector in np.multiply(scales, unit_vector)]
-    coordinates = [center + scaled_vector for scaled_vector in scaled]
-    return coordinates
+    def threshold_x(value):
+        return max(0, min(value, image.shape[1] - 1))
 
+    slicers = [
+        (
+            slice(threshold_y(y - grid_square_offset_lower), threshold_y(y + grid_square_offset_upper) + 1),
+            slice(threshold_x(x - grid_square_offset_lower), threshold_x(x + grid_square_offset_upper) + 1)
+        )
+        for x, y in coordinates]
 
-def compute_descriptor(image, vector):
-    keypoint = cv2.KeyPoint(*vector, _size=31.0)
-    describer = cv2.xfeatures2d.BriefDescriptorExtractor_create(bytes=DESCRIPTOR_SIZE)
-    descriptor = describer.compute(image, [keypoint])[1]
-    return descriptor
+    return [image[y_slicer, x_slicer].mean() for y_slicer, x_slicer in slicers]
 
-
-def compute_surrounding_distances(center, vector, image):
-    surrounding_coordinates = compute_surrounding_coordinates(center, vector)
-
-    surrounding_descriptors = list(map(p(compute_descriptor, image), surrounding_coordinates))
-    center_descriptor = compute_descriptor(image, center)
-
-    distances = [compute_difference(center_descriptor, surrounding_descriptor)
-                 for surrounding_descriptor in surrounding_descriptors]
-    return tuple(distances)
-
-
-def compute_difference(descriptor1, descriptor2):
-    if descriptor1 is None or descriptor2 is None:
-        return DESCRIPTOR_SIZE * 8
-
-    return cv2.norm(descriptor1, descriptor2, normType=cv2.HAMMING_NORM_TYPE)
-
-
-def compute_normalizer(distances, n_levels=5):
+def compute_normalizer(values, n_levels=5):
     percentiles = np.linspace(0, 100, n_levels + 1)
-    cutoffs = np.percentile(distances, percentiles)
+    cutoffs = np.percentile(values, percentiles)
     ranges = [range(int(start), int(stop + 1)) for start, stop in zip(cutoffs, cutoffs[1:])]
 
     def normalizer(value):
@@ -71,24 +51,48 @@ def compute_normalizer(distances, n_levels=5):
     return normalizer
 
 
-def normalize_distances(distances):
-    normalizer = compute_normalizer(distances)
+def compute_keypoint_signature(keypoint, image, square_offset=3):
+    num_points_wide = square_offset * 2 + 1
+    grid_width = num_points_wide * keypoint.size
 
-    return tuple(map(normalizer, distances))
+    p = max(2, int(0.5 + grid_width / (2 * num_points_wide)))
+    grid_coordinates = compute_grid_coordinates(int(keypoint.pt[0]), int(keypoint.pt[1]), int(keypoint.size), square_offset)
+    grid_averages = np.array(compute_grid_averages(grid_coordinates, image, p), dtype=int).reshape((num_points_wide, num_points_wide))
+
+    def wrap(index, stop):
+        return 0 if index >= stop else index
+
+    def neighbors(A, y, x):
+        upper = (wrap(y - 1, A.shape[0]), wrap(x, A.shape[1]))
+        left = (wrap(y, A.shape[0]), wrap(x - 1, A.shape[1]))
+        right = (wrap(y, A.shape[0]), wrap(x + 1, A.shape[1]))
+        lower = (wrap(y + 1, A.shape[0]), wrap(x, A.shape[1]))
+        coordinates = [upper, left, right, lower]
+        values = [A[coordinate] for coordinate in coordinates]
+
+        return np.array(values)
+
+    print(grid_averages.shape)
+
+    neighbors_by_grid_point = [neighbors(grid_averages, x, y) for y in range(grid_averages.shape[0]) for x in range(grid_averages.shape[1])]
+    brightness_differences = [difference + 255 for neighbors, grid_average
+                              in zip(neighbors_by_grid_point, grid_averages.flatten())
+                              for difference in neighbors - grid_average]
+    normalizer = compute_normalizer(brightness_differences)
+
+    normalized_differences = tuple(map(normalizer, brightness_differences))
+    print(len(normalized_differences))
+
+    return normalized_differences
 
 
 def compute_keypoint_signatures(image, keypoints):
     image_with_keypoints = cv2.drawKeypoints(image, keypoints, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS, outImage=None)
 
-    plt.imshow(image_with_keypoints)
-    plt.show()
+    #plt.imshow(image_with_keypoints)
+    #plt.show()
 
-    keypoint_coordinates = [np.array(keypoint.pt) for keypoint in keypoints]
-    means_by_keypoint = compute_neighbor_means(keypoint_coordinates)
+    signatures_by_keypoint = [compute_keypoint_signature(keypoint, image) for keypoint in keypoints]
 
-    surrounding_distances_by_keypoint = [compute_surrounding_distances(kp_coords, mean, image)
-                                         for kp_coords, mean in zip(keypoint_coordinates, means_by_keypoint)]
-
-    normalized_distances_by_keypoint = list(map(normalize_distances, surrounding_distances_by_keypoint))
-    return normalized_distances_by_keypoint
+    return signatures_by_keypoint
 
